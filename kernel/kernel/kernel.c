9b9a2f7bc3fc7d74d99ae7aab7888f5187f12d4a
Battleconxxx <sudhanp2004@gmail.com>
2025-06-10 12:48:52 +0530

    improved printf and toolchain

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -5,4 +5,5 @@
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+	printf("int: %d , char: %c , String: %s , hex: %x", 1234, 'U', "Batman", 1234);
 }
--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -4,6 +4,71 @@
 #include <stdio.h>
 #include <string.h>
 
+int int_to_string(int num, char *buffer){
+	char temp[12];
+	int i=0, is_negative=0;
+
+	if(num == 0){
+		buffer[0] = '0';
+		buffer[1] = '\0';
+		return 1;
+	}
+
+	if(num < 0){
+		is_negative = 1;
+		num=-num;
+	}
+
+	while(num && i<11){
+		temp[i++] = (num%10) + '0';
+		num/=10;
+	}	
+
+	if(is_negative){
+		temp[i++] = '-';
+	}
+
+	for(int j=0; j<i; j++){
+		buffer[j] = temp[i-j-1];
+	}
+
+	buffer[i] = '\0';
+	return i;
+}
+
+
+int int_to_hex(unsigned int num, char* buffer){
+
+	const char *hex_digits = "0123456789abcdef";
+	char temp[9];
+
+	int i=0;
+
+	if(num == 0){
+		buffer[0] = '0';
+		buffer[1] = 'x';
+		buffer[2] = '0';
+		buffer[3] = '\0';
+		return 3;
+	}
+
+	while(num && i<8){
+		temp[i++] = hex_digits[num % 16];
+		num >>=4;
+	}
+
+	buffer[0] = '0';
+	buffer[1] = 'x';
+
+	for(int j=0;j<i;j++){
+		buffer[2+j] = temp[i-j-1];
+	}
+
+	buffer[2+i] = '\0';
+	return 2+i;
+}
+
+
 static bool print(const char* data, size_t length) {
 	const unsigned char* bytes = (const unsigned char*) data;
 	for (size_t i = 0; i < length; i++)
@@ -61,7 +126,31 @@ int printf(const char* restrict format, ...) {
 			if (!print(str, len))
 				return -1;
 			written += len;
-		} else {
+		} else if (*format == 'd') {
+			format++;
+			const int num = va_arg(parameters, int);
+			char buffer[12];
+			size_t len = int_to_string(num, buffer);
+			if (maxrem < len) {
+				// TODO: Set errno to EOVERFLOW.
+				return -1;
+			}
+			if (!print(buffer, len))
+				return -1;
+			written += len;
+		} else if (*format == 'x') {
+			format++;
+			const unsigned int num = va_arg(parameters,unsigned int);
+			char buffer[9];
+			size_t len = int_to_hex(num, buffer);
+			if (maxrem < len) {
+				// TODO: Set errno to EOVERFLOW.
+				return -1;
+			}
+			if (!print(buffer, len))
+				return -1;
+			written += len;
+		}else {
 			format = format_begun_at;
 			size_t len = strlen(format);
 			if (maxrem < len) {
